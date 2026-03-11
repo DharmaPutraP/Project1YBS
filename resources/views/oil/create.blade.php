74e732f6926c47f93b847ddedb259043e03977b8
USERYBS <dharma22ti@mahasiswa.pcr.ac.id>
2026-03-11 01:56:20 -0700

    oil losses deploy

--- resources/views/oil/create.blade.php
+++ resources/views/oil/create.blade.php
@@ -166,20 +166,6 @@
                             @enderror
                         </div>
                     </div>
-
-                    {{-- Parameter Lain --}}
-                    <!-- <div>
-                        <label for="parameter_lain" class="block text-sm font-medium text-gray-700 mb-2">
-                            Parameter Lain <span class="text-gray-400 text-xs">(opsional)</span>
-                        </label>
-                        <textarea name="parameter_lain" id="parameter_lain" rows="2"
-                            placeholder="Catatan atau parameter tambahan lainnya..."
-                            class="w-full px-4 py-2 border border-gray-300 rounded-lg text-sm transition focus:outline-none focus:ring-2 focus:ring-blue-500
-                                   @error('parameter_lain') border-red-400 bg-red-50 @enderror">{{ old('parameter_lain') }}</textarea>
-                        @error('parameter_lain')
-                            <p class="mt-1 text-xs text-red-600">{{ $message }}</p>
-                        @enderror
-                    </div> -->
                 </div>
             </div>
 
